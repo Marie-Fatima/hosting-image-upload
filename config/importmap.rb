13c88365c2014f006f8fc16a495a58353f64296d
marie beliben <mbeliben@icloud.com>
2024-11-29 09:16:48 +0100

    Scaffold article

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
@@ -5,3 +8,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
